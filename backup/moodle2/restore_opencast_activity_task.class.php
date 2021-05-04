e857aec1d495392e5a68d20e525aafd50d732f65
Tamara Gunkel <tamara.nrw@web.de>
2021-05-04 11:50:52 +0200

    fix moodle  code checker issues

--- backup/moodle2/restore_opencast_activity_task.class.php
+++ backup/moodle2/restore_opencast_activity_task.class.php
@@ -50,7 +50,7 @@ class restore_opencast_activity_task extends restore_activity_task {
      * Define the contents in the activity that must be
      * processed by the link decoder
      */
-    static public function define_decode_contents() {
+    public static function define_decode_contents() {
         $contents = array();
 
         $contents[] = new restore_decode_content('opencast', array('intro'), 'opencast');
@@ -62,7 +62,7 @@ class restore_opencast_activity_task extends restore_activity_task {
      * Define the decoding rules for links belonging
      * to the activity to be executed by the link decoder
      */
-    static public function define_decode_rules() {
+    public static function define_decode_rules() {
         $rules = array();
 
         $rules[] = new restore_decode_rule('OPENCASTINDEX', '/mod/opencast/index.php?id=$1', 'course');
@@ -79,7 +79,7 @@ class restore_opencast_activity_task extends restore_activity_task {
      * opencast logs. It must return one array
      * of {@see restore_log_rule} objects
      */
-    static public function define_restore_log_rules() {
+    public static function define_restore_log_rules() {
         $rules = array();
 
         $rules[] = new restore_log_rule('opencast', 'add', 'view.php?id={course_module}', '{opencast}');
@@ -99,7 +99,7 @@ class restore_opencast_activity_task extends restore_activity_task {
      * by the restore final task, but are defined here at
      * activity level. All them are rules not linked to any module instance (cmid = 0)
      */
-    static public function define_restore_log_rules_for_course() {
+    public static function define_restore_log_rules_for_course() {
         $rules = array();
 
         $rules[] = new restore_log_rule('opencast', 'view all', 'index.php?id={course}', null);
