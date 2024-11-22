911a80d52230f7718ed1e212cfa861017485bc5d
ferishili <zamanifarbod2@gmail.com>
2024-11-22 12:45:51 +0100

    code improvements

--- classes/local/output_helper.php
+++ classes/local/output_helper.php
@@ -138,19 +138,16 @@ class output_helper {
             return;
         }
 
-        // Find aspect-ratio if there is only one video track.
+        // Find aspect-ratio of the first video track.
+        $wrapperattrs = [];
         if (!empty($data['streams']) && !empty($data['streams'][0]['sources'])) {
             $sources = $data['streams'][0]['sources'];
             $res = $sources[array_key_first($sources)][0]['res'];
             $resolution = $res['w'] . '/' . $res['h'];
-            echo \html_writer::start_div('player-wrapper', ['style' => '--aspect-ratio:' . $resolution]);
-        } else {
-            echo \html_writer::start_div('player-wrapper');
+            $wrapperattrs['style'] = '--aspect-ratio:' . $resolution;
         }
 
-        if (empty($data['streams'])) {
-            \core\notification::error(get_string('erroremptystreamsources', 'mod_opencast'));
-        }
+        echo \html_writer::start_div('player-wrapper', $wrapperattrs);
 
         echo '<iframe src="player.html" id="player-iframe" class="mod-opencast-paella-player" allowfullscreen"></iframe>';
         echo \html_writer::end_div();
